0c2ef119a244b5cb65cb79d1633d700dfcb0c91a
DavidTeHe <davidtehe@163.com>
2022-09-01 21:29:00 +0800

    Initial Commit

---
+++ SomeNiceViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SomeNiceViews
+//
+//  Created by 何特 on 2022/09/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SomeNiceViews/SomeNiceViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeNiceViewsApp.swift
+//  SomeNiceViews
+//
+//  Created by 何特 on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct SomeNiceViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
